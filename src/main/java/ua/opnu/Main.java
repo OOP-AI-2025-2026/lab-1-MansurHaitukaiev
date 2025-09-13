0ff2fa9bd64cb0ee9fe3a5ac3c90c5bc4ec36abd
MansurHaitukaiev <9101447@stud.op.edu.ua>
2025-09-13 15:31:56 +0200

    Lab 1 completed

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -8,31 +8,32 @@ public class Main {
     // ======== Logical operations ========
 
     /**
-     * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+     * Given two temperatures,
+     * return true if one is less than 0 and the other is greater than 100.
      * Example:
      * icyHot(120, -1) → true
      * icyHot(-1, 120) → true
      * icyHot(2, 120) → false
      */
     public boolean icyHot(int temp1, int temp2) {
-        // TODO: write method body
-        return false;
+        return (temp1 > 100 && temp2 < 0) || (temp2 > 100 && temp1 < 0);
     }
 
     /**
-     * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+     * Given 2 int values,
+     * return true if either of them is in the range 10..20 inclusive.
      * Example:
      * in1020(12, 99) → true
      * in1020(21, 12) → true
      * in1020(8, 99) → false
      */
     public boolean in1020(int a, int b) {
-        // TODO: write method body
-        return false;
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
     }
 
     /**
-     * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1
+     * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+     * Given 3 int values, return true if 1
      * or more of them are teen.
      * Example:
      * hasTeen(13, 20, 10) → true
@@ -40,49 +41,50 @@ public class Main {
      * hasTeen(20, 10, 13) → true
      */
     public boolean hasTeen(int a, int b, int c) {
-        // TODO: write method body
-        return false;
+        return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
     }
 
     // ======== Boolean expressions ========
 
     /**
-     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
-     * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+     * The parameter weekday is true if it is a weekday,
+     * and the parameter vacation is true if we are on vacation.
+     * We sleep in if it is not a weekday or we're on vacation.
+     * Return true if we sleep in.
      * Example:
      * sleepIn(false, false) → true
      * sleepIn(true, false) → false
      * sleepIn(false, true) → true
      */
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        // TODO: write method body
-        return false;
+        return !weekday || vacation;
     }
 
     /**
-     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
-     * We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     * We have two monkeys, a and b,
+     * and the parameters aSmile and bSmile indicate if each is smiling.
+     * We are in trouble if they are both smiling or if neither of them is smiling.
+     * Return true if we are in trouble.
      * Example:
      * monkeyTrouble(true, true) → true
      * monkeyTrouble(false, false) → true
      * monkeyTrouble(true, false) → false
      */
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        // TODO: write method body
-        return false;
+        return (aSmile && bSmile) || (!aSmile && !bSmile);
     }
 
     /**
-     * Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative"
-     * is true, then return true only if both are negative.
+     * Given 2 int values, return true if one is negative and one is positive.
+     * Except if the parameter "negative"
+     *      * is true, then return true only if both are negative.
      * Example:
      * posNeg(1, -1, false) → true
      * posNeg(-1, 1, false) → true
      * posNeg(-4, -5, true) → true
      */
     public boolean posNeg(int a, int b, boolean negative) {
-        // TODO: write method body
-        return false;
+        return (a > 0 && b < 0 && !negative) || (a < 0 && b > 0 && !negative) || (a < 0 && b < 0 && negative);
     }
 
     // ======== Loops and Arrays ========
@@ -95,12 +97,18 @@ public class Main {
      * arrayCount9([1, 9, 9, 3, 9]) → 3
      */
     public int arrayCount9(int[] nums) {
-        // TODO: write method body
-        return 0;
+        int count = 0;
+        for (int n : nums){
+            if (n == 9) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
-     * Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+     * Given an array of ints,
+     * return true if one of the first 4 elements in the array is a 9.
      * The array length may be less than 4.
      * Example:
      * arrayFront9([1, 2, 9, 3, 4]) → true
@@ -108,7 +116,11 @@ public class Main {
      * arrayFront9([1, 2, 3, 4, 5]) → false
      */
     public boolean arrayFront9(int[] nums) {
-        // TODO: write method body
+        for (int i = 0; i < nums.length && i < 4; i++){
+            if (nums[i] == 9) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -120,26 +132,32 @@ public class Main {
      * array123([1, 1, 2, 1, 2, 3]) → true
      */
     public boolean array123(int[] nums) {
-        // TODO: write method body
+        for (int i = 0; i < nums.length - 2; i++){
+            if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) {
+                return true;
+            }
+        }
         return false;
     }
 
     // ======== Strings ========
 
     /**
-     * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     * Given a string name, e.g. "Bob",
+     * return a greeting of the form "Hello Bob!".
      * Example:
      * helloName("Bob") → "Hello Bob!"
      * helloName("Alice") → "Hello Alice!"
      * helloName("X") → "Hello X!"
      */
     public String helloName(String name) {
-        // TODO: write method body
-        return null;
+        return "Hello " + name + "!";
     }
 
     /**
-     * Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding"
+     * Given a string of any length,
+     * return a new string where the last 2 chars,
+     * if present, are swapped, so "coding"
      * yields "codign".
      * Example:
      * lastTwo("coding") → "codign"
@@ -147,20 +165,25 @@ public class Main {
      * lastTwo("ab") → "ba"
      */
     public String lastTwo(String str) {
-        // TODO: write method body
-        return null;
+        if (str.length() < 2) {
+            return str;
+        }
+        int len = str.length();
+        return str.substring(0, len - 2) + str.charAt(len - 1) + str.charAt(len - 2);
     }
 
     /**
-     * Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+     * Given a string of even length,
+     * return a string made of the middle two chars,
+     * so the string "string" yields "ri".
      * The string length will be at least 2.
      * middleTwo("string") → "ri"
      * middleTwo("code") → "od"
      * middleTwo("Practice") → "ct"
      */
     public String middleTwo(String str) {
-        // TODO: write method body
-        return null;
+        int middle = str.length() / 2;
+        return str.substring(middle - 1, middle + 1);
     }
 
 
